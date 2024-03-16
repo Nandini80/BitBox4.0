93a76795dd9ce31151629c8136de61907e7f545d
Nandini <nandinijindal010@gmail.com>
2024-03-16 11:31:43 +0530

    features

--- Server/config/cloudinary.js
+++ Server/config/cloudinary.js
@@ -12,16 +12,12 @@ const uploadController = {};
 
 uploadController.uploadImage = async (req, res) => {
   try {
-    // Check if there is a file in the request
-    console.log(req.files.image);
 
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).json({ message: "No files were uploaded." });
     }
-
-    const file = req.files.image; // Assuming the file input field is named "image"
-
-    // Upload image to Cloudinary
+    const file = req.files.image;
+    console.log(req.files.image);
     const result = await cloudinary.uploader.upload(file.tempFilePath);
      console.log(result);
 
